32a5b76c2f8df3df7c88cdfda0599acf74269fb8
Wanderson Alves <wandersonalwes@hotmail.com>
2024-02-03 14:30:12 -0300

    style: improve the list of participants on mobile

--- frontend/components/participants.tsx
+++ frontend/components/participants.tsx
@@ -4,7 +4,7 @@ import { ScrollArea } from './ui/scroll-area'
 export const Participants = () => {
   return (
     <ScrollArea className="w-full">
-      <div className="flex-1 p-4 grid grid-cols-2 gap-3 place-content-start">
+      <div className="flex-1 p-4 grid grid-cols-1 sm:grid-cols-2 gap-3 place-content-start">
         <Participant name="Wanderson" />
         <Participant name="Miguel" />
       </div>
