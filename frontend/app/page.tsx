a808e147ab7b659b92a53b18b2c4ea663d80b1db
Wanderson Alves <wandersonalwes@hotmail.com>
2024-02-03 11:49:06 -0300

    chore: add shadcn/ui setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main>
       <h1>myroom</h1>
+
+      <Button>Começar</Button>
     </main>
   )
 }
